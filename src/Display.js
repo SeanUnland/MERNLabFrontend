73bf32bf7efd4b0112c3893a4582706465150eef
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-10-21 09:58:02 -0400

    updated display.js

--- src/Display.js
+++ src/Display.js
@@ -3,35 +3,59 @@ import React from "react";
 const Display = (props) => {
   const { cats } = props;
 
-  const loaded = () => (
-    <div style={{ textAlign: "center" }}>
-      {cats.map((cat) => (
-        <article>
-          <img src={cat.img} alt="" />
-          <h1>{cat.name}</h1>
-          <h3>{cat.age}</h3>
-          <button
-            onClick={() => {
-              props.selectCat(cat);
-              props.history.push("/edit");
-            }}
-          >
-            Edit
-          </button>
-          <button
-            onClick={() => {
-              props.deleteCat(cat);
-            }}
-          >
-            Delete
-          </button>
-        </article>
-      ))}
-    </div>
+  return (
+    <>
+      <article>
+        <img src={cats.img} alt="" />
+        <h1>{cats.name}</h1>
+        <h3>{cats.age}</h3>
+        <button
+          onClick={() => {
+            props.selectCat(cats);
+            props.history.push("/edit");
+          }}
+        >
+          Edit
+        </button>
+        <button
+          onClick={() => {
+            props.deleteCat(cats);
+          }}
+        >
+          Delete
+        </button>
+      </article>
+    </>
   );
 
-  return cats > 0 ? loaded() : <h1>Loading...</h1>;
-  //   return loaded();
+  //   const loaded = () => (
+  //     <div style={{ textAlign: "center" }}>
+  //       {cats.map((cat) => (
+  //         <article>
+  //           <img src={cat.img} alt="" />
+  //           <h1>{cat.name}</h1>
+  //           <h3>{cat.age}</h3>
+  //           <button
+  //             onClick={() => {
+  //               props.selectCat(cat);
+  //               props.history.push("/edit");
+  //             }}
+  //           >
+  //             Edit
+  //           </button>
+  //           <button
+  //             onClick={() => {
+  //               props.deleteCat(cat);
+  //             }}
+  //           >
+  //             Delete
+  //           </button>
+  //         </article>
+  //       ))}
+  //     </div>
+  //   );
+
+  //   return cats > 0 ? loaded() : <h1>Loading...</h1>;
 };
 
 export default Display;
